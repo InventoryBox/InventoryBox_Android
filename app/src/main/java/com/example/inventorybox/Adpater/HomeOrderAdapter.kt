ef1a89fb1ca7ca5507bc84ed2083ff92c7fec637
cse0616 <cse061605035@gmail.com>
2020-07-04 17:48:38 +0900

    11-home baseLayout : 재고확인 추가

--- app/src/main/java/com/example/inventorybox/Adpater/HomeOrderAdapter.kt
+++ app/src/main/java/com/example/inventorybox/Adpater/HomeOrderAdapter.kt
@@ -2,19 +2,18 @@ package com.example.inventorybox.Adpater
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.inventorybox.Data.HomeOrderData
-import com.example.inventorybox.HomeOrderViewHoler
+import com.example.inventorybox.HomeOrderViewHolder
 import com.example.inventorybox.R
 
-class HomeOrderAdapter(private val context: Context) : RecyclerView.Adapter<HomeOrderViewHoler>() {
+class HomeOrderAdapter(private val context: Context) : RecyclerView.Adapter<HomeOrderViewHolder>() {
     var datas = mutableListOf<HomeOrderData>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeOrderViewHoler {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeOrderViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_home_orderlist, parent, false)
-        return HomeOrderViewHoler(
+        return HomeOrderViewHolder(
             view
         )
     }
@@ -23,7 +22,7 @@ class HomeOrderAdapter(private val context: Context) : RecyclerView.Adapter<Home
         return datas.size
     }
 
-    override fun onBindViewHolder(holder: HomeOrderViewHoler, position: Int) {
+    override fun onBindViewHolder(holder: HomeOrderViewHolder, position: Int) {
         holder.bind(datas[position])
 
     }
