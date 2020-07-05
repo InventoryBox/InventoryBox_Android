b78e34e718b45b8d9538eb6f04d42c9a50fdeb2b
cse0616 <cse061605035@gmail.com>
2020-07-05 17:07:57 +0900

    11-home baseLayout : 뷰 바뀐것들 수정

--- app/src/main/java/com/example/inventorybox/HomeTodayOrderViewHolder.kt
+++ app/src/main/java/com/example/inventorybox/HomeTodayOrderViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.inventorybox
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -9,8 +10,10 @@ import com.example.inventorybox.Data.HomeTodayOrderData
 class HomeTodayOrderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val index = itemView.findViewById<ConstraintLayout>(R.id.rv_home_today_container)
     val name = itemView.findViewById<TextView>(R.id.tv_home_today)
+    val check = itemView.findViewById<ImageView>(R.id.iv_home_today_check)
 
     fun bind(homeTodayOrderData: HomeTodayOrderData){
         name.text = homeTodayOrderData.name
+
     }
 }
--- app/src/main/java/com/example/inventorybox/adapter/HomeOrderAdapter.kt
+++ app/src/main/java/com/example/inventorybox/adapter/HomeOrderAdapter.kt
@@ -2,11 +2,19 @@ package com.example.inventorybox.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.inventorybox.Data.HomeOrderData
-import com.example.inventorybox.HomeOrderViewHolder
+import com.example.inventorybox.Data.HomeTodayOrderData
 import com.example.inventorybox.R
+import kotlinx.android.synthetic.main.item_home_today_order.view.*
 
 class HomeOrderAdapter(private val context: Context) : RecyclerView.Adapter<HomeOrderViewHolder>() {
     var datas = mutableListOf<HomeOrderData>()
@@ -25,7 +33,32 @@ class HomeOrderAdapter(private val context: Context) : RecyclerView.Adapter<Home
     override fun onBindViewHolder(holder: HomeOrderViewHolder, position: Int) {
         holder.bind(datas[position])
 
+
     }
 
 
+}
+
+class HomeOrderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private var mData: ArrayList<String>? = null
+
+    private var isChecked = 0
+
+    val index = itemView.findViewById<ConstraintLayout>(R.id.rv_home_container)
+    val img = itemView.findViewById<ImageView>(R.id.img_rv_product)
+    val name = itemView.findViewById<TextView>(R.id.tv_rv_product)
+    val count = itemView.findViewById<TextView>(R.id.tv_rv_count)
+
+    val check_box = itemView.findViewById<CheckBox>(R.id.checkBox)
+    val btn_more = itemView.findViewById<ImageButton>(R.id.btn_rv_more)
+
+
+    fun bind(homeOrderData: HomeOrderData){
+        Glide.with(itemView).load(homeOrderData.img).into(img)
+        name.text = homeOrderData.name
+        count.text = homeOrderData.count.toString()
+    }
+
+    
 }
