0823687dc2985e73c1505989e79be3c6b13ed9ba
cse0616 <cse061605035@gmail.com>
2020-07-07 18:51:16 +0900

    30 : data package 오류 수정

--- app/src/main/java/com/example/inventorybox/adapter/GraphSingleGraphAdapter.kt
+++ app/src/main/java/com/example/inventorybox/adapter/GraphSingleGraphAdapter.kt
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.inventorybox.data.GraphSingleData
 import com.example.inventorybox.MainActivity
-import com.example.inventorybox.data.GraphSingleData
 import com.example.inventorybox.R
 import com.example.inventorybox.fragment.GraphDetail
 import com.example.inventorybox.fragment.HomeFragment
