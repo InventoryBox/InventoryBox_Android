bb5bd2c046993f7810c1dd357e6fa6044f767a73
GaYoung <kians2@naver.com>
2020-07-08 00:23:18 +0900

    26-Graph datepicker data fragment 에 연결하기 완료

--- app/src/main/java/com/example/inventorybox/etc/DatePickerWeekOnly.kt
+++ app/src/main/java/com/example/inventorybox/etc/DatePickerWeekOnly.kt
@@ -1,4 +1,4 @@
-package com.example.inventorybox
+package com.example.inventorybox.etc
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
@@ -9,21 +9,24 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.Window
 import android.widget.Button
-import android.widget.NumberPicker
 import androidx.fragment.app.DialogFragment
+import com.example.inventorybox.R
+import com.super_rabbit.wheel_picker.WheelPicker
 import java.util.*
 
-class DatePickerWeekOnly : DialogFragment(){
+class DatePickerWeekOnly() : DialogFragment(){
+
     private val MAX_YEAR = 2020
     private val MIN_YEAR = 2010
+    lateinit var picker_month :WheelPicker
+    lateinit var picker_year :WheelPicker
+    private var listener : DatePickerDialog.OnDateSetListener? = null
 
-    private lateinit var listener : DatePickerDialog.OnDateSetListener
     val cal : Calendar = Calendar.getInstance()
     fun setListener(listener : DatePickerDialog.OnDateSetListener){
-        this.listener=listener
-    }
+        this.listener = listener
+}
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
@@ -33,38 +36,37 @@ class DatePickerWeekOnly : DialogFragment(){
         val dialog = inflater.inflate(R.layout.graph_datepicker_custom_week_only, null)
 
         val btn = dialog.findViewById<Button>(R.id.graph_custom_dialog_btn)
-        val picker_month = dialog.findViewById<NumberPicker>(R.id.picker_month)
-        val picker_year = dialog.findViewById<NumberPicker>(R.id.picker_year)
 
+        picker_month=dialog.findViewById<WheelPicker>(R.id.picker_month)
+        picker_year=dialog.findViewById<WheelPicker>(R.id.picker_year)
+
+        // 선택 버튼 누르면 리스너에 data 전달해주기
         btn.setOnClickListener {
-            listener.onDateSet(null, picker_year.value, picker_month.value, 0)
-            this.dialog!!.cancel()
+            listener?.onDateSet(null, picker_year.getCurrentItem().toInt(), picker_month.getCurrentItem().toInt(), 0)
+            this.dialog?.cancel()
         }
 
         // picker 의 min 값과 max값 설정
-        picker_month.minValue=1
-        picker_month.maxValue=12
-        picker_month.value= cal.get(Calendar.MONTH)+1
+        picker_month.setMin(1)
+        picker_month.setMax(12)
+        picker_year.setMin(MIN_YEAR)
+        picker_year.setMax(MAX_YEAR)
 
-        val year = cal.get(Calendar.YEAR)
-        picker_year.minValue=MIN_YEAR
-        picker_year.maxValue=MAX_YEAR
-        picker_year.value = year
+        //현재 날짜 기준으로 스크
+        picker_month.scrollToValue((cal.get(Calendar.MONTH)+1).toString())
+        picker_year.scrollToValue(cal.get(Calendar.YEAR).toString())
 
         builder.setView(dialog)
         return builder.create()
     }
-//
+
+    // dialog 를 밑에 꽉차게 뜨도록
     override fun onResume() {
-    val width = resources.getDimensionPixelSize(R.dimen.popup_width)
-    val height = resources.getDimensionPixelSize(R.dimen.popup_height)
     dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
-//        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         dialog?.window?.setGravity(Gravity.FILL_HORIZONTAL)
         dialog?.window?.
         setGravity(Gravity.BOTTOM)
     dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        dialog?.window?.setLayout(width, height)
         super.onResume()
 
 
--- app/src/main/java/com/example/inventorybox/fragment/GraphDetail.kt
+++ app/src/main/java/com/example/inventorybox/fragment/GraphDetail.kt
@@ -2,27 +2,36 @@ package com.example.inventorybox.fragment
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.service.autofill.Dataset
 import android.util.Log
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.NumberPicker
-import android.widget.Toast
-import com.example.inventorybox.DatePickerWeekOnly
+import android.widget.DatePicker
+import com.example.inventorybox.etc.DatePickerWeekOnly
 import com.example.inventorybox.R
-import com.super_rabbit.wheel_picker.WheelAdapter
 import kotlinx.android.synthetic.main.fragment_graph_detail.*
-import kotlinx.android.synthetic.main.graph_datepicker_custom_week_only.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 class GraphDetail : Fragment() {
-    val d : DatePickerDialog.OnDateSetListener = DatePickerDialog.OnDateSetListener { datePicker, year, month, day_of_month ->
-        Log.d("dialogTest","year = $year, month = $month, date = $day_of_month")
+
+    var cur_month = ""
+    var cur_year = ""
+
+    val listener: DatePickerDialog.OnDateSetListener = object  : DatePickerDialog.OnDateSetListener{
+        override fun onDateSet(p0: DatePicker?, year: Int, month: Int, p3: Int) {
+            Log.d("datepicker","year = $year, month = $month")
+        }
     }
+//    val month_listener: OnValueChangeListener = object : OnValueChangeListener{
+//        override fun onValueChange(picker: WheelPicker, oldVal: String, newVal: String) {
+//            cur_month=newVal
+//        }
+//    }
+//    val year_listener :OnValueChangeListener =object : OnValueChangeListener{
+//        override fun onValueChange(picker: WheelPicker, oldVal: String, newVal: String) {
+//            cur_year=newVal
+//        }
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        graph_detail_btn_back.setOnClickListener {
@@ -47,54 +56,18 @@ class GraphDetail : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         button4.setOnClickListener {
 
+
+
+
             val pd = DatePickerWeekOnly()
-            pd.setListener(d)
+//            pd.setListener(d)
             pd.show(requireFragmentManager(), "test")
+//            pd.setListener(listener_month=month_listener, listener_year = year_listener)
+            pd.setListener(listener)
 
         }
     }
 
 
 
-}
-class WPDayPickerAdapter : WheelAdapter {
-    //get item value based on item position in wheel
-    override fun getValue(position: Int): String {
-        if (position == 0)
-            return "Today"
-
-        if(position == -1)
-            return "Yesterday"
-
-        if (position == 1)
-            return "Tomorrow"
-
-        val curDate = Date(System.currentTimeMillis())
-        val rightNow = Calendar.getInstance()
-        rightNow.time = curDate;
-        rightNow.add(Calendar.DATE, position)
-
-        val simpleDateFormat = SimpleDateFormat("MMM d, yyyy")
-        return simpleDateFormat.format(rightNow.time)
-    }
-
-    //get item position based on item string value
-    override fun getPosition(vale: String): Int {
-        return 0
-    }
-
-    //return a string with the approximate longest text width, for supporting WRAP_CONTENT
-    override fun getTextWithMaximumLength(): String {
-        return "Mmm 00, 0000"
-    }
-
-    //return the maximum index
-    override fun getMaxIndex(): Int {
-        return Integer.MAX_VALUE
-    }
-
-    //return the minimum index
-    override fun getMinIndex(): Int {
-        return Integer.MIN_VALUE
-    }
 }
