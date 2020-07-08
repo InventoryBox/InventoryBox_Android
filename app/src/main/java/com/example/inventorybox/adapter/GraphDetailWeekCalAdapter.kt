e68a8ec8292cde3a89423b1b3d0e1e685a0bcd65
GaYoung <kians2@naver.com>
2020-07-09 01:51:31 +0900

    35-Graph Detail 주간 그래프 비교 구현 완료

--- app/src/main/java/com/example/inventorybox/adapter/GraphDetailWeekCalAdapter.kt
+++ app/src/main/java/com/example/inventorybox/adapter/GraphDetailWeekCalAdapter.kt
@@ -4,14 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.inventorybox.R
+import com.example.inventorybox.fragment.onMyChangeListener
+import com.example.inventorybox.getColorFromRes
 import kotlinx.android.synthetic.main.item_graph_detail_calendar.view.*
 
 class GraphDetailWeekCalAdapter(private val context: Context, val max_week:Int): RecyclerView.Adapter<GraphDetailWeekCalHolder>() {
 
 //  	var datas: MutableList<String> = mutableListOf()
-
+    lateinit var listener: onMyChangeListener
   	// xml file을 inflate한 후 viewHolder를 만든다.
       override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  GraphDetailWeekCalHolder{
   	    val view = LayoutInflater.from(context).inflate(R.layout.item_graph_detail_calendar, parent,false)
@@ -33,21 +36,43 @@ class GraphDetailWeekCalAdapter(private val context: Context, val max_week:Int):
           }
           holder.bind(week_name)
           holder.itemView.setOnClickListener {
+              //지금 안보이는 상태이면 보이게 만들고 visible 하게 만들기
               if(holder.isClicked){
-                  holder.itemView.setBackgroundResource(R.drawable.graph_rec9_white)
-                  holder.isClicked=false
+//                  holder.itemView.setBackgroundResource(R.drawable.graph_rec9_white)
+//                  holder.itemView.tv_week.setTextColor(holder.itemView.context.getColorFromRes(R.color.darkgrey))
+                  holder.backgroundToGrey()
+//                  holder.isClicked=false
+                  listener.onChange(position, false)
               }else{
-                  holder.itemView.setBackgroundResource(R.drawable.graph_rec9_yellow)
-                  holder.isClicked=true
+//                  holder.itemView.setBackgroundResource(R.drawable.graph_rec9_yellow)
+//                  holder.itemView.tv_week.setTextColor(holder.itemView.context.getColorFromRes(R.color.white))
+                  holder.backgroundToYellow()
+//                  holder.isClicked=true
+                  listener.onChange(position, true)
               }
 
           }
       }
+    fun set(listener : onMyChangeListener){
+        this.listener=listener
+    }
   }
 class GraphDetailWeekCalHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
     var isClicked = false
     val tv_week = itemView.tv_week
     fun bind(data : String){
         tv_week.text = data
+        //처음에는 background yellow로
+    }
+    //background 노란색으로 바꾸는
+    fun backgroundToYellow(){
+        itemView.setBackgroundResource(R.drawable.graph_rec9_yellow)
+        tv_week.setTextColor(itemView.context.getColorFromRes(R.color.white))
+        isClicked=true
+    }
+    fun backgroundToGrey(){
+        itemView.setBackgroundResource(R.drawable.graph_rec9_white)
+        tv_week.setTextColor(itemView.context.getColorFromRes(R.color.darkgrey))
+        isClicked=false
     }
  }
--- app/src/main/java/com/example/inventorybox/fragment/GraphDetail.kt
+++ app/src/main/java/com/example/inventorybox/fragment/GraphDetail.kt
@@ -5,13 +5,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import android.widget.LinearLayout
+import androidx.core.view.marginBottom
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inventorybox.FixAppBarLayoutBehavior
 import com.example.inventorybox.etc.DatePickerWeekOnly
 import com.example.inventorybox.R
 import com.example.inventorybox.adapter.GraphDetailWeekCalAdapter
+import com.example.inventorybox.adapter.GraphDetailWeekGraphAdapter
+import com.example.inventorybox.data.GraphSingleWeekData
 import kotlinx.android.synthetic.main.fragment_graph.*
 import kotlinx.android.synthetic.main.fragment_graph_detail.*
 import kotlinx.android.synthetic.main.fragment_graph_detail.cal_month
@@ -25,6 +32,8 @@ class GraphDetail : Fragment() {
     val listener: DatePickerDialog.OnDateSetListener = object  : DatePickerDialog.OnDateSetListener{
         override fun onDateSet(p0: DatePicker?, year: Int, month: Int, p3: Int) {
             Log.d("datepicker","year = $year, month = $month")
+            cal_month.text=if(month<10) "0"+month.toString() else month.toString()
+            cal_year.text=year.toString()
         }
     }
 //    val month_listener: OnValueChangeListener = object : OnValueChangeListener{
@@ -48,11 +57,16 @@ class GraphDetail : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //product name 설정
+        tv_product_name.text="우유"
+
         // 뒤로가기 버튼
         btn_back.setOnClickListener {
             val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
             transaction.remove(this).commit()
         }
+
         val cal : Calendar = Calendar.getInstance()
 
         val format = SimpleDateFormat("MM")
@@ -68,9 +82,74 @@ class GraphDetail : Fragment() {
             pd.setListener(listener)
 
         }
-        graph_detail_week_cal.adapter=GraphDetailWeekCalAdapter(view.context, 5)
+
+        val cal_adapter=GraphDetailWeekCalAdapter(view.context, 5)
+        graph_detail_week_cal.adapter=cal_adapter
+
+        val weeks_adapter = GraphDetailWeekGraphAdapter(view.context)
+        //onlyfortest
+        val datas = createDatas()
+        weeks_adapter.datas=datas
+        rv_graph_weeks.adapter=weeks_adapter
+
+
+        val listener = object : onMyChangeListener{
+            override fun onChange(position: Int, isVisible: Boolean) {
+                // 보이게 만들고 싶은 거면,
+                if(isVisible){
+                    val item_view = rv_graph_weeks.layoutManager?.findViewByPosition(position)
+                    item_view?.visibility = View.VISIBLE
+//                rv_graph_weeks.layoutManager?.findViewByPosition(position)?.layoutParams = RecyclerView.LayoutParams(0,0)
+                    val params = item_view?.layoutParams
+                    params?.height= LinearLayout.LayoutParams.WRAP_CONTENT
+                    item_view?.layoutParams=params
+                    view.invalidate()
+                    weeks_adapter.notifyDataSetChanged()
+                    weeks_adapter.notifyItemChanged(position)
+                }else{
+                    val item_view = rv_graph_weeks.layoutManager?.findViewByPosition(position)
+                    item_view?.visibility = View.GONE
+//                rv_graph_weeks.layoutManager?.findViewByPosition(position)?.layoutParams = RecyclerView.LayoutParams(0,0)
+                    val params = item_view?.layoutParams
+                    params?.height=0
+                    item_view?.layoutParams=params
+
+
+                    view.invalidate()
+                    weeks_adapter.notifyDataSetChanged()
+                    weeks_adapter.notifyItemChanged(position)
+                }
+            }
+        }
+        cal_adapter.set(listener)
+
+
+
+    }
+
+    private fun createDatas() : MutableList<GraphSingleWeekData>{
+        return mutableListOf<GraphSingleWeekData>(
+            GraphSingleWeekData( "5월 1일",
+            "5월 7일",
+            arrayListOf(-1, 11, 0, 3, 5, -1, 0)),
+            GraphSingleWeekData( "5월 8일",
+                "5월 15일",
+                arrayListOf(-1, 2, 0, 3, 5, 7, 0)),
+            GraphSingleWeekData( "5월 15일",
+                "5월 21일",
+                arrayListOf(-1, 11, 2, 3, 5, -1, 0)),
+            GraphSingleWeekData( "5월 22일",
+                "5월 28일",
+                arrayListOf(-1, 2, 0, 3, 5, -1, 0)),
+            GraphSingleWeekData( "5월 29일",
+                "5월 31일",
+                arrayListOf(-1, 11, 0, 3, 5, -1, 0))
+        )
     }
 
 
-
+}
+interface onMyChangeListener{
+    // position 에 있는 item을 isvisible 하게 만들지
+    fun onChange(position: Int, isVisible : Boolean)
 }
