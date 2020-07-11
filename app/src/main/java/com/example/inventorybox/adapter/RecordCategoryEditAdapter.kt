e6ab6c4d6cc0bf7c6ab57f85e193ad286d724ef2
hjh1161514 <hjh1161514@naver.com>
2020-07-11 21:20:12 +0900

    54-RecordDatePicker

---
+++ app/src/main/java/com/example/inventorybox/adapter/RecordCategoryEditAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.inventorybox.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inventorybox.R
+import com.example.inventorybox.ViewHolder.RecordCategoryVH
+import com.example.inventorybox.data.RecordCategoryData
+
+class RecordCategoryEditAdapter(private val context: Context) : RecyclerView.Adapter<RecordCategoryVH>() {
+    var datas = mutableListOf<RecordCategoryData>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordCategoryVH {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_record_edit, parent, false)
+        return RecordCategoryVH(
+            view
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    override fun onBindViewHolder(holder: RecordCategoryVH, position: Int) {
+        holder.bind(datas[position])
+
+    }
+
+}
