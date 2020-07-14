e74f5d1092ad1c130ca4df545f66c03570f3ba3e
cse0616 <cse061605035@gmail.com>
2020-07-14 17:19:12 +0900

    74-login

--- app/src/main/java/com/example/inventorybox/network/POST/PostLoginResponse.kt
+++ app/src/main/java/com/example/inventorybox/network/POST/PostLoginResponse.kt
@@ -1,10 +1,12 @@
 package com.example.inventorybox.network.POST
 
-import android.service.autofill.UserData
-
 data class PostLoginResponse (
     val status : Int,
     val success : Boolean,
     val message : String,
-    val data : UserData?    //사용자 인증 정보를 저장할 토큰값 (null값을 가질 수 있음)
+    val data : SomeData?    //사용자 인증 정보를 저장할 토큰값 (null값을 가질 수 있음)
+)
+
+data class SomeData(
+    val jwt : String
 )
