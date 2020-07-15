6dde9ff0233364282b075fcd6638a364d3b89d6a
cse0616 <cse061605035@gmail.com>
2020-07-15 19:15:43 +0900

    Home 서버 연결 완료

--- app/src/main/java/com/example/inventorybox/fragment/GraphFragment.kt
+++ app/src/main/java/com/example/inventorybox/fragment/GraphFragment.kt
@@ -122,8 +122,8 @@ import java.util.*
 
 //                datas_cate = it.data.categoryInfo.toMutableList()
                  for(data in it.data.categoryInfo){
-                     datas_cate.add(data)
-                 }
+                 datas_cate.add(data)
+             }
                  category_adapter.datas = datas_cate
                  category_adapter.notifyDataSetChanged()
 
